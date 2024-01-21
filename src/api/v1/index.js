6224301b471c0df540522e90d8f4a001933bb46c
yochirolee <yleecruz@gmail.com>
2024-01-21 11:59:36 -0500

    cors added 2

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,16 +1,14 @@
 import express from "express";
-import userRouter from "./Routes/user.routes.js";
 import trackingRouter from "./Routes/tracking.routes.js";
-import uploadExcelRouter from "./Routes/upload.excel.routes.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	res.status(200).json({ message: "CTEnvios Tracking API - V1, contact: soporte@ctenvios.com 👋🌎🌍🌏" });
+	res
+		.status(200)
+		.json({ message: "CTEnvios Tracking API - V1, contact: soporte@ctenvios.com 👋🌎🌍🌏" });
 });
 
-router.use("/users", userRouter);
 router.use("/tracking", trackingRouter);
-router.use("/upload-excel", uploadExcelRouter);
 
 export default router;
