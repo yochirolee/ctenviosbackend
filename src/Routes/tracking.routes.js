70582e73aa38125b4b72d4dddef3a409a4c97558
yochirolee <yleecruz@gmail.com>
2024-01-22 16:03:49 -0500

    search hbl2

--- src/Routes/tracking.routes.js
+++ src/Routes/tracking.routes.js
@@ -14,7 +14,7 @@ router.get("/hbl/:hbl", async (req, res) => {
 	if (!req.params.hbl) return res.json({ message: "HBL is required" });
 	console.log(req.params);
 	const pack = await mySqlService.packages.getPackageByHBL(req.params.hbl);
-	console.log(pack)
+	console.log(pack);
 	if (pack.length === 0) return res.json({ message: "Package not found" });
 
 	const tracking = await prisma.tracking.findUnique({
@@ -35,40 +35,40 @@ router.get("/hbl/:hbl", async (req, res) => {
 
 		packages: pack.map((pack) => {
 			return {
-				hbl: pack.HBL,
-				quantity: pack.Quantity,
-				weight: pack.ProductWeight,
+				hbl: pack?.HBL,
+				quantity: pack?.Quantity,
+				weight: pack?.ProductWeight,
 				status: tracking?.status ? tracking.status : "New",
-				description: pack.Description,
-				palletId: pack.PalletId,
+				description: pack?.Description,
+				palletId: pack?.PalletId,
 
 				trackingHistory: [
 					{
 						location: "Entregado",
-						createdAt: tracking.find((track) => track.hbl === pack.HBL)?.deliveredDate,
+						createdAt: tracking.find((track) => track?.hbl === pack?.HBL)?.deliveredDate,
 					},
 					{
 						location: "En Traslado",
-						createdAt: tracking.find((track) => track.hbl === pack.HBL)?.transfertDate,
+						createdAt: tracking.find((track) => track?.hbl === pack?.HBL)?.transfertDate,
 					},
 					{
 						location: "Listo para Traslado",
-						createdAt: tracking.find((track) => track.hbl === pack.HBL)?.pendingTransfertDate,
+						createdAt: tracking.find((track) => track?.hbl === pack?.HBL)?.pendingTransfertDate,
 					},
 
 					{
 						location: "Aduana Cuba",
-						createdAt: tracking.find((track) => track.hbl === pack.HBL)?.customsDate,
+						createdAt: tracking.find((track) => track?.hbl === pack?.HBL)?.customsDate,
 					},
 					{
 						location: "Puerto del Mariel",
-						createdAt: tracking.find((track) => track.hbl === pack.HBL)?.portDate,
+						createdAt: tracking.find((track) => track?.hbl === pack?.HBL)?.portDate,
 					},
 					{ location: "En Contenedor", createdAt: pack?.ContainerDate ? pack.ContainerDate : null },
-					{ location: "En Pallet", createdAt: pack?.PalletDate ? pack.PalletDate : null },
+					{ location: "En Pallet", createdAt: pack?.PalletDate ? pack?.PalletDate : null },
 
 					{
-						createdAt: pack.InvoiceDate,
+						createdAt: pack?.InvoiceDate,
 						location: "Facturado",
 					},
 				],
